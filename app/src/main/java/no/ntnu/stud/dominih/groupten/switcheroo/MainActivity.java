43dd91b53433ae938993c8ebe394682291eac099
Dominik Huber <dominih@stud.ntnu.no>
2018-04-21 10:26:41 +0200

    Added database integration + DB "hello world"

--- app/src/main/java/no/ntnu/stud/dominih/groupten/switcheroo/MainActivity.java
+++ app/src/main/java/no/ntnu/stud/dominih/groupten/switcheroo/MainActivity.java
@@ -1,7 +1,12 @@
 package no.ntnu.stud.dominih.groupten.switcheroo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Simple database hello world
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference switcherooRef = database.getReference("switcheroo");
+
+        String random = UUID.randomUUID().toString();
+
+        DatabaseReference newChild = switcherooRef.push();
+        newChild.setValue(random);
+
     }
 }
