69f0372462bd6abc7e5058b32c8d56be1948c105
Dominik Huber <dominih@stud.ntnu.no>
2018-04-22 11:40:39 +0200

    Refactored and shortened the DrawFragment

--- app/src/main/java/no/ntnu/stud/dominih/groupten/switcheroo/DrawFragment.java
+++ app/src/main/java/no/ntnu/stud/dominih/groupten/switcheroo/DrawFragment.java
@@ -15,74 +15,59 @@ import android.widget.TextView;
 public class DrawFragment extends Fragment implements View.OnClickListener {
 
 
+    public static final String KEY_HAS_TEXT = "MDSFMSDFDS";
+    public static final String KEY_TEXT_CAPTION = "MDFKSFMKSDFDSF";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
     private DrawView drawView;
     private Button clearButton;
     private Button sendButton;
     private GameActivity parent;
     private TextView textToDraw;
 
-
-
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    //private OnFragmentInteractionListener mListener;
-
     public DrawFragment() {
         // Required empty public constructor
     }
 
-    /*
-    // TODO: Rename and change types and number of parameters
-    public static DrawFragment newInstance(String param1, String param2) {
-        DrawFragment fragment = new DrawFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-    */
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
-
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        View view = inflater.inflate(R.layout.fragment_drawing, container, false);
         parent = (GameActivity) getActivity();
 
-        View _view = inflater.inflate(R.layout.fragment_drawing, container, false);
-        drawView = _view.findViewById(R.id.drawing);
-        clearButton = _view.findViewById(R.id.clear_button);
-        clearButton.setOnClickListener(this);
-        textToDraw = _view.findViewById(R.id.txt_to_draw);
+        setupViews(view);
+        loadText();
+        setupClickBehaviour(view);
 
-        //TODO PASS TEXT TO textToDraw
-        textToDraw.setText("PLACE HERE THE TEXT RECEIVED FROM THE SERVER");
-        //
-        sendButton = _view.findViewById(R.id.send_button);
+        return view;
+
+    }
+
+    private void setupClickBehaviour(View view) {
+        sendButton = view.findViewById(R.id.send_button);
         sendButton.setOnClickListener(this);
+    }
 
-        return _view;
+    private void loadText() {
+        Bundle arguments = getArguments();
+        String text = "You're first! Draw something inspiring.";
+        if (arguments != null) {
 
+            boolean hasText = arguments.getBoolean(KEY_HAS_TEXT);
+            if (hasText) {
+
+                text = arguments.getString(KEY_TEXT_CAPTION);
+
+            }
+
+        }
+        textToDraw.setText(text);
+    }
+
+    private void setupViews(View view) {
+        drawView = view.findViewById(R.id.drawing);
+        clearButton = view.findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(this);
+        textToDraw = view.findViewById(R.id.txt_to_draw);
     }
 
 
@@ -103,35 +88,6 @@ public class DrawFragment extends Fragment implements View.OnClickListener {
         }
 
 
-    }
-
-    /**
-     * Sets the textView to the answer of the previous player
-     * @param answer
-     */
-
-    public void setTextToDraw(Bundle answer){
-
-        String textString = answer.getString("textToDraw");
-        textToDraw.setText(textString);
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-/*
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }*/
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
     }
 
 }
