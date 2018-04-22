f0bd2e8e380d7d6c7194652e387f42ade03a97cf
Dominik Huber <dominih@stud.ntnu.no>
2018-04-22 16:24:41 +0200

    Another logic fix in MainActivity #timeForCoffee

--- app/src/main/java/no/ntnu/stud/dominih/groupten/switcheroo/MainActivity.java
+++ app/src/main/java/no/ntnu/stud/dominih/groupten/switcheroo/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
 
                     setUsername(text);
                     dialog.dismiss();
-                    showMainMenuFragment();
+                    checkCameraPermissions();
 
                 }
 
